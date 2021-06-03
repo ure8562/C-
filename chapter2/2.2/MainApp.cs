31cd14cafe2cf911da113a9cd2370aae4efce599
ure8562 <55536830+ure8562@users.noreply.github.com>
2021-06-03 14:46:37 +0900

    Add files via upload

---
+++ chapter2/2.2/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace chapter2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            //프로그램 시작부
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+
+            WriteLine("hello, {0}!", args[0]);
+        }
+        
+    }
+}
