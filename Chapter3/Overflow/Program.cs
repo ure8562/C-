35c78d1875bfe053580c6dca22af25e307cc9d8c
ure8562 <55536830+ure8562@users.noreply.github.com>
2021-06-04 16:33:25 +0900

    Add files via upload

---
+++ Chapter3/Overflow/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SignedUnsigned
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            byte a = 255;
+            sbyte b = (sbyte)a;
+
+            Console.WriteLine($"a={a}");
+            Console.WriteLine($"b={b}");
+        }
+    }
+}
