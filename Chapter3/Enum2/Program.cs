6ff958aaf3a541c3b7d92a0493ea87c30819d56e
ure8562 <55536830+ure8562@users.noreply.github.com>
2021-06-09 17:19:46 +0900

    Add files via upload

---
+++ Chapter3/Enum2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enum2
+{
+    class MainApp
+    {
+        enum DialogResult { YES, NO, CANCLE, CONFIRM, OK }
+
+        static void Main(string[] args)
+        {
+            DialogResult result = DialogResult.YES;
+
+            Console.WriteLine(result == DialogResult.YES);
+            Console.WriteLine(result == DialogResult.NO);
+            Console.WriteLine(result == DialogResult.CANCLE);
+            Console.WriteLine(result == DialogResult.CONFIRM);
+            Console.WriteLine(result == DialogResult.OK);
+        }
+    }
+}
---
+++ Chapter3/Enum3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enum3
+{
+    class MainApp
+    {
+        enum DialogResult { YES = 10, NO, CANCLE, CONFIRM = 50, OK }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine((int)DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CANCLE);
+            Console.WriteLine((int)DialogResult.CONFIRM);
+            Console.WriteLine((int)DialogResult.OK);
+        }
+    }
+}
