d9b5f9bd00e54baf7f80663179ee2cc810fef908
ure8562 <55536830+ure8562@users.noreply.github.com>
2021-06-09 17:08:56 +0900

    Add files via upload

---
+++ Chapter3/Enum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enum
+{
+    class MainApp
+    {
+        enum DialogResult { YES, NO, CANCLE, CONFIRM, OK }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine((int)DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CANCLE);
+            Console.WriteLine((int)DialogResult.CONFIRM);
+            Console.WriteLine((int)DialogResult.OK);
+        }
+    }
+}
---
+++ Chapter3/StringNumberConversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringNumberConversion
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            string b = a.ToString();
+            Console.WriteLine(b);
+
+            float c = 3.14f;
+            string d = c.ToString();
+            Console.WriteLine(d);
+
+            string e = "123456";
+            int f = Convert.ToInt32(e);
+            Console.WriteLine(f);
+
+            string g = "1.23456";
+            float h = float.Parse(g);
+            Console.Write(h);
+
+
+        }
+    }
+}
