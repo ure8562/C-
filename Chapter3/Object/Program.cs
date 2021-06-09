98f38419ac261c55e1a0362042035db302a8499f
ure8562 <55536830+ure8562@users.noreply.github.com>
2021-06-09 17:04:26 +0900

    Add files via upload

---
+++ Chapter3/Object/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Objcet
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            object a = 123;
+            object b = 3.141592653589793238462643383279m;
+            object c = true;
+            object d = "안녕하세요.";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+        }
+    }
+}
