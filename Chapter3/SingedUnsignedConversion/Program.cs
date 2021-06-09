479289f7a6be7dedff0ed10d698b33e529dd25e6
ure8562 <55536830+ure8562@users.noreply.github.com>
2021-06-09 17:06:26 +0900

    Add files via upload

---
+++ Chapter3/SingedUnsignedConversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SignedUnsignedConversion
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 500;
+            Console.WriteLine(a);
+
+            uint b = (uint)a;
+            Console.WriteLine(b);
+
+            int x = -30;
+            Console.WriteLine(x);
+
+            uint y = (uint)x;       //언더플로우(Underflow) 발생
+            Console.WriteLine(y);
+
+        }
+    }
+}
