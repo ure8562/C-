10d71be069a835e296264adad1a0784dd78fd442
ure8562 <sjwnrdjtj@naver.com>
2021-06-09 17:55:26 +0900

    이것이 C#이다 Nullable 예제

---
+++ Chapter3/Nullable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Nullable
+{
+    class MainApp
+    {
+        enum DialogResult { YES = 10, NO, CANCLE, CONFIRM = 50, OK }
+
+        static void Main(string[] args)
+        {
+
+            int? a = null;
+
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a != null);
+
+            a = 3;
+
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a != null);
+            Console.WriteLine(a.Value);
+        }
+    }
+}
