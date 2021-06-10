081e81f888232edbda0e2ea18b817cac2221b79b
ure8562 <sjwnrdjtj@naver.com>
2021-06-10 13:16:39 +0900

    이것이 C#이다 StringFormatBasic 예제

---
+++ Chapter3/StringFormatBasic/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+using System;
+
+namespace StringFormatBasic
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string fmt = "{0,-20}{1,-15}{2,30}";
+
+            WriteLine(fmt, "Publish", "Author", "Title");
+            WriteLine(fmt, "Marvel", "Stan Lee", "Iron Man");
+            WriteLine(fmt, "Hanbit", "Sanghyun Park", "This is C#");
+            WriteLine(fmt, "Prentice Hall", "K&R", "The C Programming Language");
+
+        }
+    }
+}
