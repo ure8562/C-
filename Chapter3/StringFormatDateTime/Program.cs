f9d131038a4f208417e8f838d2a006fdf826ee41
ure8562 <sjwnrdjtj@naver.com>
2021-06-10 13:33:02 +0900

    이것이 C#이다. 3장 예제 StringFormatDatetime

---
+++ Chapter3/StringFormatDateTime/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+using System;
+using System.Globalization;
+
+namespace StringFormatDateTime
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 3, 23, 18, 22);
+
+            WriteLine("12시간 형식: {0:yyyy-MM-dd tt hh:mm:ss (ddd)}", dt);
+            WriteLine("12시간 형식: {0:yyyy-MM-dd HH:mm:ss (dddd)}", dt);
+
+            CultureInfo ciKo = new CultureInfo("ko-KR");
+            WriteLine();
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciKo));
+            WriteLine(dt.ToString("yyyy-MM-dd tt HH:mm:ss (dddd)", ciKo));
+            WriteLine(dt.ToString(ciKo));
+
+            CultureInfo ciEn = new CultureInfo("en-US");
+            WriteLine();
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciEn));
+            WriteLine(dt.ToString("yyyy-MM-dd tt HH:mm:ss (dddd)", ciEn));
+            WriteLine(dt.ToString(ciEn));
+        }
+    }
+}
