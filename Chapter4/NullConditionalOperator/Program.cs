98f59b7c32806c7244dd1162ff26a389c97dcc0c
ure8562 <sjwnrdjtj@naver.com>
2021-06-11 09:49:35 +0900

    이것ㅇ이것이 C#이다. 4장 예제 NullConditionalOperator

---
+++ Chapter4/NullConditionalOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+using System.Collections;
+
+namespace NullConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = null;
+            a?.Add("야구");
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+
+            a = new ArrayList();
+            a?.Add("야구");
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+        }
+    }
+}
