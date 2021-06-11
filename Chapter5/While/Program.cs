d916196ab11093c1484e9e99b7c5ed37a324c0d8
ure8562 <sjwnrdjtj@naver.com>
2021-06-11 15:16:53 +0900

    이것이 C#이다. 5장 예제 While

---
+++ Chapter5/While/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            while(i >0)
+            {
+                Console.WriteLine($"i : {i--}");
+            }
+        }
+    }
+}
