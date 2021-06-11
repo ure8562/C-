a0b137e056044aa1ddd995a6851b8b1d5e4b2c8e
ure8562 <sjwnrdjtj@naver.com>
2021-06-11 16:13:53 +0900

    이것이 C#이다. 4장 예제 InfiniteWhile

---
+++ Chapter5/InfiniteWhile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InfiniteWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(true)
+            {
+                Console.WriteLine(i++);
+            }
+        }
+    }
+}
