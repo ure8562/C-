dd8025b115b98d15f6732fcaf4516667425dc7ae
ure8562 <sjwnrdjtj@naver.com>
2021-06-11 09:42:35 +0900

    이것이 C#이다. 4장 예제 ConditionalOperator

---
+++ Chapter4/ConditionalOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = (10 % 2) == 0 ? "짝수" : "홀수";
+
+            Console.Write(result);
+
+        }
+    }
+}
