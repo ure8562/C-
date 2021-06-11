951eebf2984e4c759bd92b2e4c11214fcc57d403
ure8562 <sjwnrdjtj@naver.com>
2021-06-11 16:59:39 +0900

    이것이 C#이다. 6장 예제 Calculator

---
+++ Chapter6/Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Method
+{
+    class Calculator
+    {
+        public static int Puls(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator Cal = new Calculator();
+            int result = Calculator.Puls(3, 4);
+            Console.WriteLine(result);
+
+            result = Cal.Minus(5, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
