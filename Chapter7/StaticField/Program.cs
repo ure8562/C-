be0a1ea66b3b3a13fbdcd83e245d38cf76c5db08
ure8562 <sjwnrdjtj@naver.com>
2021-06-15 11:04:43 +0900

    이것이 C#이다. 7장 예제 StaticField

---
+++ Chapter7/StaticField/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StaticField
+{
+    class Grobal
+    {
+        public static int Count = 0;
+    }
+
+    class ClassA
+    {
+        public ClassA()
+        {
+            Grobal.Count++;
+        }
+    }
+
+    class ClassB
+    {
+        public ClassB()
+        {
+            Grobal.Count++;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Grobal.Count : {Grobal.Count}");
+
+            new ClassA();
+            new ClassA();
+            new ClassB();
+            new ClassB();
+            
+            Console.WriteLine($"Grobal.Count : {Grobal.Count}");
+
+        }
+    }
+}
