c2652b883a017d246c19e054ef1878de562f57ee
ure8562 <sjwnrdjtj@naver.com>
2021-06-15 17:59:42 +0900

    이것이 C#이다. 7장 예제 ReadnonlyFields

---
+++ Chapter7/ReadonlyFields/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReadonlyFields
+{
+    class Configuration
+    {
+        private readonly int min;
+        private readonly int max;
+
+
+        public Configuration(int v1, int v2)
+        {
+            min = v1;
+            max = v2;
+        }
+
+        public void ChangeMax(int newMax)
+        {
+            max = newMax;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Configuration c = new Configuration(100, 10);
+        }
+    }
+}
