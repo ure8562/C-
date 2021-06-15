f8f4c4b5933d4da13c2ad4313b3550ccc8a0a496
ure8562 <sjwnrdjtj@naver.com>
2021-06-15 15:30:58 +0900

    이것이 C#이다. 7장 예제 ArmorSuite

---
+++ Chapter7/Overriding/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Overriding
+{
+    class ArmorSuite
+    {
+        public virtual void Initialize()
+        {
+            Console.WriteLine("Armored");
+        }
+    }
+    class IronMan : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Repulsor Rays Armed");
+        }
+    }
+    class WarMachine : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Double-Barrel Cannons Armed");
+            Console.WriteLine("Micro-Rocket Launcher Armed");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating ArmorSuite...");
+            ArmorSuite armorsuite = new ArmorSuite();
+            armorsuite.Initialize();
+
+            Console.WriteLine("\nCreating IronMan...");
+            ArmorSuite ironman = new IronMan();
+            ironman.Initialize();
+
+            Console.WriteLine("\nCreating WarMachine...");
+            ArmorSuite warmachine = new WarMachine();
+            warmachine.Initialize();
+        }
+    }
+}
