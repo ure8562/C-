42e1c8e1208d043b68d8f2bdacb1e3bf12dc4442
ure8562 <sjwnrdjtj@naver.com>
2021-06-15 15:48:09 +0900

    이것이 C#이다. 7장 예제 SealedMethod

---
+++ Chapter7/SealedMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SealedMethod
+{
+    class Base
+    {
+        public virtual void SealMe()
+        {
+
+        }
+    }
+    class Derived : Base
+    {
+        public sealed override void SealMe()
+        {
+        }
+    }
+    class test : Base
+    {
+        public override void SealMe()
+        {
+        }
+    }
+    class derived_test : test
+    {
+        public override void SealMe()
+        { }
+    }
+    class WantToOverried : Derived
+    {
+        public override void SealMe() 
+        { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
