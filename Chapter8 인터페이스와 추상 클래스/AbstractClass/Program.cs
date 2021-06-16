603ba34177fffefef1f3f6d2dea65165463c62b8
ure8562 <sjwnrdjtj@naver.com>
2021-06-16 19:19:23 +0900

    이것이 C#이다. 8장 예제 AbstractClass

---
+++ Chapter8 인터페이스와 추상 클래스/AbstractClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    abstract class AbstractBase
+    {
+        protected void PrivateMethodA()
+        {
+            Console.WriteLine("AbstractBase.PrivateMethodA()");
+        }
+
+        public void PublicMethodA()
+        {
+            Console.WriteLine("AbstractBase.PulicMethodA()");
+        }
+
+        public abstract void AbstractMethodA();
+
+    }
+
+    class Derived : AbstractBase
+    {
+        public override void AbstractMethodA()
+        {
+            Console.WriteLine("Derived.AbstractMethodA()");
+            PrivateMethodA();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractBase obj = new Derived();
+            obj.AbstractMethodA();
+            obj.PublicMethodA();
+        }
+    }
+}
---
+++ Chapter8 인터페이스와 추상 클래스/DefaultImplementation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DefaultImplementation
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+        
+        void WriteError(string error)   // 새로운 메소드 추가   
+        {
+            WriteLog($"Error : {error}");
+        }
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine($"{DateTime.Now.ToLocalTime()}, {message}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new ConsoleLogger();
+            logger.WriteLog("System Up");
+            logger.WriteError("System Fail");
+
+            ConsoleLogger clogger = new ConsoleLogger();
+            clogger.WriteLog("System Up");  //OK
+            //clogger.WriteError("System Fail");    Error
+
+        }
+    }
+}
