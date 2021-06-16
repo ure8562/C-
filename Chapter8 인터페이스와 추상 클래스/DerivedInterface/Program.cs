516c85af3b62ba8d2aeddef93386132684e104f8
ure8562 <sjwnrdjtj@naver.com>
2021-06-16 18:03:24 +0900

    이것이 C#이다. 8장 예제 DerivedInterface

---
+++ Chapter8 인터페이스와 추상 클래스/DerivedInterface/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DerivedInterface
+{
+    interface ILogger
+    {
+        void WriteLog(string meassge);
+    }
+    interface IFormattableLogger : ILogger
+    {
+        void WriteLog(string format, params Object[] args);
+    }
+
+    class ConsoleLogger2 : IFormattableLogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine("{0} {1}",
+                DateTime.Now.ToLocalTime(), message);
+        }
+
+        public void WriteLog(string format, params object[] args)
+        {
+            string message = String.Format(format, args);
+            Console.WriteLine("{0} {1}",
+                DateTime.Now.ToLocalTime(), message);
+        }
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFormattableLogger logger = new ConsoleLogger2();
+            logger.WriteLog("The world is not flat");
+            logger.WriteLog("{0} + {1} = {2}", 1, 1, 2);
+        }
+    }
+}
