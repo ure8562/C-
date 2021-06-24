7dee8a7d3715da32edc1fa36bde1eccf7e7449be
ure8562 <sjwnrdjtj@naver.com>
2021-06-24 14:21:57 +0900

    이것이 C#이다. 9장 예제 AnnonymousType

---
+++ Chapter9 프로퍼티/AnnonymousType/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AnnonymousType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new { Name = "박상현", Age = 123 };
+            Console.WriteLine($"Name:{a.Name}, Age:{a.Age}");
+
+            var b = new { Subject = "수학", Scores = new int[] { 90, 80, 70, 60, } };
+
+            Console.Write($"Subject:{b.Subject}, Score : ");
+            foreach (var score in b.Scores)
+                Console.Write($"{score} ");
+
+            Console.WriteLine();
+        }
+    }
+}
