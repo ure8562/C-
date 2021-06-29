88ee7e8c42a0013d8cb55021c402a45ee18fc50c
ure8562 <sjwnrdjtj@naver.com>
2021-06-29 16:10:13 +0900

    이것이 C#이다. 12장 예제 ThrowExpression

---
+++ Chapter12 예외처리하기/ThrowExpression/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ThrowExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int? a = null;
+                int b = a ?? throw new ArgumentNullException();
+            }
+            catch(ArgumentNullException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                int[] array = new[] { 1, 2, 3 };
+                int index = 4;
+                int value = array[index >= 0 && index < 3 ? index : throw new IndexOutOfRangeException()];
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
