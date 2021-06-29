d60df2e73ac7aa0fc6964456327921b72067bb98
ure8562 <sjwnrdjtj@naver.com>
2021-06-29 14:01:07 +0900

    이것이 C#이다. 11장 예제 usingGenericStack

---
+++ Chapter11 일반화 프로그래밍/usingGenericStack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace usingGenericStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
