39105029734a056e6a56635088847da958edff97
ure8562 <sjwnrdjtj@naver.com>
2021-06-29 16:22:45 +0900

    이것이 C#이다. 12장 예제 Finally

---
+++ Chapter12 예외처리하기/Finally/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Finally
+{
+    class Program
+    {
+        static int Divide(int dividend, int divisor)
+        {
+            try
+            {
+                Console.WriteLine("Divide() 시작");
+                return dividend / divisor;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Divide() 예외 발생");
+                throw e;
+            }
+            finally
+            {
+                Console.WriteLine("끝");
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("제수를 입력하세요 : ");
+                String temp = Console.ReadLine();
+                int dividend = Convert.ToInt32(temp);
+
+                Console.Write("피제수를 입력하세요 : ");
+                temp = Console.ReadLine();
+                int divsor = Convert.ToInt32(temp);
+
+                Console.WriteLine("{0}/{1} = {2}", dividend, divsor, Divide(dividend, divsor));
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("에러 : " + e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("에러 : " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("프로그램을 종료합니다.");
+            }
+        }
+    }
+}
