3cf4218030a68f8f1a882aa8a0b7524c5e435acf
ure8562 <sjwnrdjtj@naver.com>
2021-06-29 16:39:15 +0900

    이것이 C#이다. 12장 예제 StackTrace

---
+++ Chapter12 예외처리하기/StackTrace/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StackTrace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 1;
+                Console.WriteLine(3 / --a);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
