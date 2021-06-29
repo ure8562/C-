1fbca0d5724bd76ede7658d9316e4f264664d529
ure8562 <sjwnrdjtj@naver.com>
2021-06-29 15:41:44 +0900

    이것이 C#이다. 12장 예제 KillingProgram

---
+++ Chapter12 예외처리하기/KillingProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KillingProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.WriteLine("종료");
+
+        }
+    }
+}
