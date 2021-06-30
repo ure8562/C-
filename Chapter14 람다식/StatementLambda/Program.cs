095bebc8023480808a6c4829abb18907154845a7
ure8562 <sjwnrdjtj@naver.com>
2021-06-30 16:21:53 +0900

    이것이 C#이다. 14장 예제 StatementLambda, SimpleLambda

---
+++ Chapter14 람다식/StatementLambda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StatementLambda
+{
+    delegate string Concatenate(string[] arg);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Concatenate concat =
+                (arr) =>
+                {
+                    string result = "";
+                    foreach (string s in arr)
+                    {
+                        result += s;
+                    }
+                    return result;
+                };
+            Console.WriteLine(concat(args));
+        }
+    }
+}
---
+++ Chapter14 람다식/simpleLambda/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace simpleLambda
+{
+    class Program
+    {
+        delegate int Calculate(int a, int b);
+        static void Main(string[] args)
+        {
+            Calculate calc = (a, b) => a + b;
+            Console.WriteLine($"{3} + {4} : {calc(3, 4)}");
+        }
+    }
+}
