1913f692444e9e7faf6dd1720a1a0513ecb06c46
ure8562 <sjwnrdjtj@naver.com>
2021-07-01 11:26:37 +0900

    이것이 C#이다. 14장 예제 ExpressionBodiedMember

---
+++ Chapter14 람다식/ExpressionTreeViaaLambda/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionTreeViaaLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<int, int, int>> expression =
+                (a, b) => 1 * 2 + (a - b);
+            Func<int, int, int> func = expression.Compile();
+
+            Console.WriteLine($"1*2+({7}-{8}) = {func(7, 8)}");
+        }
+    }
+}
