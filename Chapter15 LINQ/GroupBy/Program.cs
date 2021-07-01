03797a53b07338efd476f5eb850af070f52a6d22
ure8562 <sjwnrdjtj@naver.com>
2021-07-01 16:06:45 +0900

    이것이 C#이다. 15장 예제 GroupBy

---
+++ Chapter15 LINQ/GroupBy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace GroupBy
+{   
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrProfile =
+            {
+                new Profile(){Name="정우성", Height=186},
+                new Profile(){Name="김태희", Height=158},
+                new Profile(){Name="고현정", Height=172},
+                new Profile(){Name="이문세", Height=178},
+                new Profile(){Name="하하", Height=171}
+            };
+
+            var listProfile = from Profile in arrProfile
+                              orderby Profile.Height
+                              group Profile by Profile.Height < 175 into g
+                              select new { GruopKey = g.Key, Profile = g };
+
+            foreach (var Group in listProfile)
+            {
+                Console.WriteLine($"- 175 미만? : {Group.GruopKey}");
+                foreach (var profile in Group.Profile)
+                {
+                    Console.WriteLine($">>>{profile.Name}, {profile.Height}");
+                }
+            }
+        }
+    }
+}
