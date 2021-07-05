219c725082e335fa38ee2d950daa3af5f0401cc0
ure8562 <sjwnrdjtj@naver.com>
2021-07-05 14:17:16 +0900

    이것이 C#이다. 17장 예제 DuckTyping

---
+++ Chapter17 dynamic 형식/DuckTyping/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DuckTyping
+{
+    class Duck
+    {
+        public void Walk()
+        {
+            Console.WriteLine(this.GetType() + ".Walk");
+        }
+        public void Swim()
+        {
+            Console.WriteLine(this.GetType() + ".swim");
+        }
+        public void Quack()
+        {
+            Console.WriteLine(this.GetType() + ".Quack");
+        }
+    }
+
+    class Mallard : Duck
+    {
+
+    }
+
+    class Robot
+    {
+        public void Walk()
+        {
+            Console.WriteLine("Robot.Walk");
+        }
+        public void Swim()
+        {
+            Console.WriteLine("Robot.swim");
+        }
+        public void Quack()
+        {
+            Console.WriteLine("Robot.Quack");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic[] arr = new dynamic[] { new Duck(), new Mallard(), new Robot() };
+
+            foreach (dynamic duck in arr)
+            {
+                Console.WriteLine(duck.GetType());
+                duck.Walk();
+                duck.Swim();
+                duck.Quack();
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
