86ef6f63be6278e2777ba7ce699b3943d972c778
ure8562 <sjwnrdjtj@naver.com>
2021-07-05 13:15:16 +0900

    이것이 C#이다. 16장 예제 BasicAttribute

---
+++ Chapter16 리플렉션과 애트리뷰트/BasicAttribute/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BasicAttribute
+{
+    class MyClass
+    {
+        [Obsolete("OldMethod는 폐기되었습니다. NewMethod를 이용하세요.")]
+        public void OldMethod()
+        {
+            Console.WriteLine("I'm old");
+        }
+
+        public void NewMethod()
+        {
+            Console.WriteLine("I'm new");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.OldMethod();
+            obj.NewMethod();
+        }
+    }
+}
