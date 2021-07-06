41bd44f1a9f2fd8f12dbd066e9d0f774f36b6f01
ure8562 <sjwnrdjtj@naver.com>
2021-07-06 16:46:41 +0900

    이것이 C#이다. 18장 예제 BasicIO

---
+++ Chapter18 파일다루기/BasicIO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace BasicIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long someValue = 0x123456789ABCDEF0;
+            Console.WriteLine("{0,-1} : 0X{1:X16}", "Original Data", someValue);
+
+            Stream outStream = new FileStream("a.dat", FileMode.Create);
+            byte[] wByte = BitConverter.GetBytes(someValue);
+
+            Console.Write("{0,-13} : ", "Byte array");
+
+            foreach (byte b in wByte)
+                Console.Write("{0:X2} ", b);
+            Console.WriteLine();
+
+            outStream.Write(wByte, 0, wByte.Length);
+            outStream.Close();
+
+            Stream inStream = new FileStream("a.dat", FileMode.Open);
+            byte[] rbyte = new byte[8];
+
+            int i = 0;
+            while (inStream.Position < inStream.Length)
+                rbyte[i++] = (byte)inStream.ReadByte();
+
+            long readValue = BitConverter.ToInt64(rbyte, 0);
+            Console.WriteLine("{0,-13} : 0x{1:X16} ", "Read Data", readValue);
+            inStream.Close();
+        }
+    }
+}
