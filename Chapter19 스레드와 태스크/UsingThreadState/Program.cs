b679046bf52fabe52a9883da87f3f109cac3b60f
ure8562 <sjwnrdjtj@naver.com>
2021-07-07 11:11:04 +0900

    이것이 C#이다. 19장 예제 UsingThreadState

---
+++ Chapter19 스레드와 태스크/UsingThreadState/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace UsingThreadState
+{
+    class Program
+    {
+        private static void PrintThreadState(ThreadState state)
+        {
+            Console.WriteLine("{0, -16} : {1}", state, (int)state);
+        }
+        static void Main(string[] args)
+        {
+            PrintThreadState(ThreadState.Running);
+
+            PrintThreadState(ThreadState.SuspendRequested);
+
+            PrintThreadState(ThreadState.Background);
+
+            PrintThreadState(ThreadState.Unstarted);
+
+            PrintThreadState(ThreadState.Stopped);
+
+            PrintThreadState(ThreadState.WaitSleepJoin);
+
+            PrintThreadState(ThreadState.Suspended);
+
+            PrintThreadState(ThreadState.AbortRequested);
+
+            PrintThreadState(ThreadState.Aborted);
+
+            PrintThreadState(ThreadState.Stopped | ThreadState.Aborted);
+
+        }
+    }
+}
