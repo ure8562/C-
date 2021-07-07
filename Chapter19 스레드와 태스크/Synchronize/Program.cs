1848eea56ca285a1dc737c59e0a11df140aafac5
ure8562 <sjwnrdjtj@naver.com>
2021-07-07 13:03:16 +0900

    이것이 C#이다. 19장 예제 Synchronize

---
+++ Chapter19 스레드와 태스크/Synchronize/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace Synchronize
+{
+    class Counter
+    {
+        const int LOOP_COUNT = 1000;
+
+        readonly object thislock;
+
+        private int count;
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public Counter()
+        {
+            thislock = new object();
+            count = 0;
+        }
+
+        public void Increase()
+        {
+            int loopCount = LOOP_COUNT;
+            while (loopCount-- > 0)
+            {
+                lock (thislock)
+                {
+                    count++;
+                }
+                Thread.Sleep(1);
+            }
+        }
+        public void Decrease()
+        {
+            int loopCount = LOOP_COUNT;
+            while (loopCount-- > 0)
+            {
+                lock (thislock)
+                {
+                    count--;
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            Thread incThread = new Thread(new ThreadStart(counter.Increase));
+            Thread decThread = new Thread(new ThreadStart(counter.Decrease));
+
+            incThread.Start();
+            decThread.Start();
+
+            incThread.Join();
+            decThread.Join();
+
+            Console.WriteLine(counter.Count);
+        }
+    }
+}
