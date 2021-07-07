fa01f8700d276c2854d187783f023e8360223bf4
ure8562 <sjwnrdjtj@naver.com>
2021-07-07 17:00:06 +0900

    이것이 C#이다. 20장 예제 SimpeWindow

---
+++ Chapter20 WinForm으로 만드는 사용자 인터페이스/SimpleWindow/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpeWindow
+{
+    class MainApp : System.Windows.Forms.Form
+    {
+        static void Main(string[] args)
+        {
+            System.Windows.Forms.Application.Run(new MainApp());
+        }
+    }
+}
