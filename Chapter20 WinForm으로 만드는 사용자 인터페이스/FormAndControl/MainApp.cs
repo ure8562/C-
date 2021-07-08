e56ce6c67ad4f8056297f544ae0c8fe370a5315d
ure8562 <sjwnrdjtj@naver.com>
2021-07-08 13:14:56 +0900

    이것이 C#이다. 20장 예제 FormAndControl

---
+++ Chapter20 WinForm으로 만드는 사용자 인터페이스/FormAndControl/MainApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FormAndControl
+{
+    class MainApp : Form
+    {
+        static void Main(string[] args)
+        {
+            Button button = new Button();
+
+            button.Text = "Click Me!";
+            button.Left = 100;
+            button.Top = 50;
+
+            button.Click += (object sender, EventArgs e) =>
+             {
+                 MessageBox.Show("딸각");
+             };
+
+            MainApp form = new MainApp();
+            form.Text = "Form & Control";
+            form.Height = 150;
+
+            form.Controls.Add(button);
+
+            Application.Run(form);
+        }
+    }
+}
