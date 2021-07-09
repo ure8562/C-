9cb77b9b7ebb9be57b237a024da9195ab52f2462
ure8562 <sjwnrdjtj@naver.com>
2021-07-09 15:14:41 +0900

    이것이 C#이다. 21장 예제 FPU/FileReceiver

---
+++ Chapter21 네트워크 프로그래밍/FUP/Body.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUP
+{
+    public class BodyRequest : ISerializable    // 파일 전송 요청 메시지 0x01
+    {
+        public long FILESIZE;
+        public byte[] FILENAME;
+
+        public BodyRequest() { }
+        public BodyRequest(byte[] bytes)
+        {
+            FILESIZE = BitConverter.ToInt64(bytes, 0);
+            FILENAME = new byte[bytes.Length - sizeof(long)];
+            Array.Copy(bytes, sizeof(long), FILENAME, 0, FILENAME.Length);
+        }
+        public byte[] GetBytes()
+        {
+            byte[] bytes = new byte[GetSize()];
+            byte[] temp = BitConverter.GetBytes(FILESIZE);
+            Array.Copy(temp, 0, bytes, 0, temp.Length);
+            Array.Copy(FILENAME, 0, bytes, temp.Length, FILENAME.Length);
+
+            return bytes;
+        }
+
+        public int GetSize()
+        {
+            return sizeof(long) + FILENAME.Length;
+        }
+
+    }
+
+    public class BodyResponse : ISerializable   // 파일 전송 요청 메시지 0x02
+    {
+        public uint MSGID;
+        public byte RESPONSE;
+        public BodyResponse() { }
+        public BodyResponse(byte[] bytes)
+        {
+            MSGID = BitConverter.ToUInt32(bytes, 0);
+            RESPONSE = bytes[4];
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] bytes = new byte[GetSize()];
+            byte[] temp = BitConverter.GetBytes(MSGID);
+            Array.Copy(temp, 0, bytes, 0, temp.Length);
+            bytes[temp.Length] = RESPONSE;
+
+            return bytes;
+        }
+
+        public int GetSize()
+        {
+            return sizeof(uint) + sizeof(byte);
+        }
+    }
+
+    public class BodyData : ISerializable   // 파일 전송 요청 메시지 0x03
+    {
+        public byte[] DATA;
+
+        public BodyData(byte[] bytes)
+        {
+            DATA = new byte[bytes.Length];
+            bytes.CopyTo(DATA, 0);
+
+        }
+
+        public byte[] GetBytes()
+        {
+            return DATA;
+        }
+
+        public int GetSize()
+        {
+            return DATA.Length;
+        }
+    }
+
+    public class BodyResult : ISerializable  // 파일 전송 요청 메시지 0x04
+    {
+        public uint MSGID;
+        public byte RESULT;
+
+        public BodyResult() { }
+        public BodyResult(byte[] bytes)
+        {
+            MSGID = BitConverter.ToUInt32(bytes, 0);
+            RESULT = bytes[4];
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] bytes = new byte[GetSize()];
+            byte[] temp = BitConverter.GetBytes(MSGID);
+            Array.Copy(temp, 0, bytes, 0, temp.Length);
+            bytes[temp.Length] = RESULT;
+
+            return bytes;
+        }
+
+        public int GetSize()
+        {
+            return sizeof(uint) + sizeof(byte);
+        }
+    }
+}
---
+++ Chapter21 네트워크 프로그래밍/FileReceiver/MainApp.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using FUP;
+
+namespace FileReceiver
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("사용법 : {0} <Directory>", Process.GetCurrentProcess().ProcessName);
+                return;
+            }
+            uint msgId = 0;
+
+            string dir = args[0];
+            if (Directory.Exists(dir) == false)
+                Directory.CreateDirectory(dir);
+
+            const int bindPort = 5425;
+            TcpListener server = null;
+            try
+            {
+                IPEndPoint localAddress = new IPEndPoint(0, bindPort);  //첫번째 인자 0이면 127.0.0.1 뿐아니라 OS에 할당되어 있는 어떤주소로도 접속가능
+
+                server = new TcpListener(localAddress);
+                server.Start();
+                Console.WriteLine("파일 업로드 서버 시작...");
+
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("클라이언트 접속 : {0}", ((IPEndPoint)client.Client.RemoteEndPoint).ToString());
+
+                    NetworkStream stream = client.GetStream();  //클라이언트가 보내온 파일 전송 요청 메시지를 수신
+
+                    Message reqMsg = MessageUtil.Receive(stream);
+                    if (reqMsg.Header.MSGTYPE != CONSTANTS.REQ_FILE_SEND)
+                    {
+                        stream.Close();
+                        client.Close();
+                        continue;
+                    }
+
+                    BodyRequest reqBody = (BodyRequest)reqMsg.Body;
+
+                    Console.WriteLine("파일 업로드 요청이 왔습니다. 수락하시겠습니까? yes/no");
+                    string answer = Console.ReadLine();
+
+                    Message rspMsg = new Message();
+                    rspMsg.Body = new BodyResponse()
+                    {
+                        MSGID = reqMsg.Header.MSGID,
+                        RESPONSE = CONSTANTS.ACCEPTED
+                    };
+                    rspMsg.Header = new Header()
+                    {
+                        MSGID = msgId++,
+                        MSGTYPE = CONSTANTS.REP_FILE_SEND,
+                        BODYLEN = (uint)rspMsg.Body.GetSize(),
+                        FRAGMENTED = CONSTANTS.NOT_FRAGMENTED,
+                        LASTMSG = CONSTANTS.LASTMSG,
+                        SEQ = 0
+                    };
+
+                    if (answer != "yes")
+                    {
+                        rspMsg.Body = new BodyResponse()
+                        {
+                            MSGID = reqMsg.Header.MSGID,
+                            RESPONSE = CONSTANTS.DENIED
+                        };
+                        MessageUtil.Send(stream, rspMsg);
+                        stream.Close();
+                        client.Close();
+
+                        continue;
+                    }
+                    else
+                    {
+                        MessageUtil.Send(stream, rspMsg);
+                    }
+
+                    Console.Write("파일 전송을 시작합니다.");
+
+                    long fileSize = reqBody.FILESIZE;
+                    string fileName = Encoding.Default.GetString(reqBody.FILENAME);
+                    FileStream file = new FileStream(dir + "\\" + fileName, FileMode.Create);   //업로드 파일 스트림 생성
+
+                    uint? dataMsgId = null;
+                    ushort prevSeq = 0;
+                    while ((reqMsg = MessageUtil.Receive(stream)) != null)
+                    {
+                        Console.Write("#");
+                        if (reqMsg.Header.MSGTYPE != CONSTANTS.FILE_SEND_DATA)
+                            break;
+
+                        if (dataMsgId == null)
+                            dataMsgId = reqMsg.Header.MSGID;
+                        else
+                        {
+                            if (dataMsgId != reqMsg.Header.MSGID)
+                                break;
+                        }
+
+                        if (prevSeq++ != reqMsg.Header.SEQ)     //메시지 순성 어긋나면 종료
+                        {
+                            Console.Write("{0}, {1}", prevSeq, reqMsg.Header.SEQ);
+                            break;
+                        }
+
+                        file.Write(reqMsg.Body.GetBytes(), 0, reqMsg.Body.GetSize());   // 전송받은 스트림을 서버에서 생성한 파일에 기록
+
+                        if (reqMsg.Header.FRAGMENTED == CONSTANTS.NOT_FRAGMENTED)   // 분할 메시지가 아니면 한번만 돌고 탈출
+                            break;
+                        if (reqMsg.Header.LASTMSG == CONSTANTS.LASTMSG) //마지막 메시지면 반복문 탈출
+                            break;
+                    }
+
+                    long recvFileSize = file.Length;
+                    file.Close();
+
+                    Console.WriteLine();
+                    Console.WriteLine("수신 파일 크기 : {0} bytes", recvFileSize);
+
+                    Message rstMsg = new Message();
+                    rstMsg.Body = new BodyResult()
+                    {
+                        MSGID = reqMsg.Header.MSGID,
+                        RESULT = CONSTANTS.SUCCESS
+                    };
+                    rstMsg.Header = new Header()
+                    {
+                        MSGID = msgId++,
+                        MSGTYPE = CONSTANTS.FILE_SEND_DATA,
+                        BODYLEN = (uint)rstMsg.Body.GetSize(),
+                        FRAGMENTED = CONSTANTS.NOT_FRAGMENTED,
+                        LASTMSG = CONSTANTS.LASTMSG,
+                        SEQ = 0
+                    };
+
+                    if (fileSize == recvFileSize)
+                        MessageUtil.Send(stream, rstMsg);   // 전송파일과 실제파일크기 비교하여 같으면 성공
+                    else
+                    {
+                        rstMsg.Body = new BodyResult()
+                        {
+                            MSGID = reqMsg.Header.MSGID,
+                            RESULT = CONSTANTS.FAIL
+                        };
+
+                        MessageUtil.Send(stream, rstMsg);   //파일크기 이상있으면 실패메시지
+                    }
+                    Console.WriteLine("파일 전송을 마쳤습니다.");
+
+                    stream.Close();
+                    client.Close();
+                }
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("서버를 종료합니다.");
+        }
+    }
+}
